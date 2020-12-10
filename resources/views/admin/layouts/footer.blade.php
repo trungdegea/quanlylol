020bd060e992341297ef4a134498656206639612
trung <18521551@gm.uit.edu.vn>
2020-12-10 21:31:31 +0700

    fix page dsgiaidau

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,9 +1,9 @@
   <!-- Main Footer -->
-  <footer class="main-footer">
+  <footer class="main-footer" style="text-align:center">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2020 <a href="#">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy;2020 <a href="#">Admin@YourLeague</a>.</strong> All rights reserved.
   </footer>
