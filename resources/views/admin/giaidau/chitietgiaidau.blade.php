926fbcbc0523274c12942f4ef2c8b486b7d78c1a
17520289 <17520289@gm.uit.edu.vn>
2020-12-21 18:25:31 +0700

    yes

--- resources/views/admin/giaidau/chitietgiaidau.blade.php
+++ resources/views/admin/giaidau/chitietgiaidau.blade.php
@@ -44,11 +44,10 @@
               <div class="col-md-3"> Số vé còn lại: <label for="" style="background-color: rgb(83, 241, 123); color:white"> {{$giaidau->SLve}}</label></div>
             </div>
             <?php
-          
                 $date=new DateTime('NOW');
                 echo $date->format('c');
             ?>
-            {{$giaidau}}
+            
             <a href="{{route("sua-giaidau.get",[$giaidau->MaGD])}}" class="btn btn-warning waves-light waves-effect" title="Sửa"><i class="fas fa-pencil-alt"></i></a>
           </div>
           
@@ -65,29 +64,6 @@
        
               
               <div class="card-body">
-                <form  method="post" action="{{route('xemdoithanhvien.post',[$giaidau->MaGD])}}">
-                  <input type="hidden" name="_token" value="{{csrf_token()}}">
-                  <div class=" row">
-                    <div class="col-md-6">
-                      <h4 class="card-title mb-4 font-size-16">Danh sách thành viên:</h4>
-                    </div>
-                    <div class="col-md-4">
-                      <label for="inputEmail3" class="col-sm-3 col-form-label">Đội:</label>
-                     <select name="locdoi" id="">
-                       <option value="#">All</option>
-                      
-                        @foreach ($doi as $d)
-                        <option value="{{$d->MaDoi}}">{{$d->TenDoi}}</option>
-                        @endforeach
-                     </select>
-                    </div>
-                    <div class="col-md-1">
-                      <button type="submit" class="btn btn-primary btn-block " name="xem" >Xem</button>
-                    </div>
-                  </div>
-                </form>
-                
-                  <input type="hidden" name="_token" value="{{csrf_token()}}">
                 <div class="card-box table-responsive dvData">
                   <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                     <thead>
@@ -108,18 +84,11 @@
                         </td>
                         <td>
                           {{$tv->TenTV}}
-                          {{-- <input type="text" class="" name="tentv{{$tv->MaTV}}" value="{{old('tentv',$tv->TenTV)}}"> --}}
+                        
                         </td>
                         <td>
                           {{$tv->ViTri}}
-                          {{-- <select name="vitri{{$tv->MaTV}}" id="">
-                            <option value="Top">Top</option>
-                            <option value="Jungle">Jungle</option>
-                            <option value="Mid">Mid</option>
-                            <option value="ADC">ADC</option>
-                            <option value="Support">Support</option>
-                            <option value="{{$tv->ViTri}}" selected>{{$tv->ViTri}}</option>
-                          </select> --}}
+                         
                         </td>
                         <td>
                           {{$arrdoi[$tv->MaDoi]}}
